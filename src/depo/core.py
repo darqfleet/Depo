29d82805c13c6ef9c0ffc38cb4ee7ae4ff0955a1
darqfleet <yaroslav.danilovsky@gmail.com>
2025-09-19 17:20:22 +0200

    added test strukture

--- src/depo/core.py
+++ src/depo/core.py
@@ -1,7 +1,4 @@
-import pathlib
 import os
-
-from src.depo.exceptions import DepoWarning
 from utils import exist_path
 from typing import List
 
@@ -31,41 +28,63 @@ class DepoItem:
 class DepoStructure:
     def __init__(self, structure: str, db=None):
         self.db = db
+        self.structure = structure
         self.depo = f'{structure}/depo'
         self.configs = f'{self.depo}/0000'
         self.validate_structure()
+
+    def __repr__(self):
+        return f'DepoStructure({self.structure})'
 
     def validate_structure(self):
         exist_path(self.depo)
         exist_path(self.configs)
 
+    def items(self) -> List[DepoItem]:
+        depo_items = self.exclude_pipeline_catalog(os.listdir(self.depo))
+        return [DepoItem(name, parent=self) for name in depo_items]
+
+    @staticmethod
+    def exclude_pipeline_catalog(depo_items: List[str]) -> List[str]:
+        depo_items.remove('0000')
+        return depo_items
+
+
+class Depo:
+    def __init__(self, structures: List[DepoStructure]):
+        self._structures = structures
+        self._current_depo = self._structures[0]
+        self._items: List[DepoItem] = []
+        self.merge_structures()
+
     def items(self):
-        items = self.delete_0000(os.listdir(self.depo))
-        return [DepoItem(name, parent=self) for name in items]
+        return self._items
 
-    def delete_0000(self, items: list) -> list:
-        items.remove('0000')
-        return items
+    def set_current_depo(self):
+        # TODO
+        pass
 
+    def add_depo(self):
+        # TODO
+        pass
 
-class DepoCombain:
-    def __init__(self, depos: List[DepoStructure]):
-        self.depos = depos
-        self.items = self.merge_structures()
+    @property
+    def current_depo(self):
+        return self._current_depo
 
-    def merge_structures(self):
-        DepoWarning('item not unique')
-        items = []
-        for index, v in enumerate(range(len(self.depos))):
-            items.extend(self.depos[index].items())
-        return items
+    def append_structures(self, depo_items: List[DepoItem]) -> None:
+        self._items.extend(depo_items)
+
+    def merge_structures(self) -> None:
+        for index, _ in enumerate(range(len(self._structures))):
+            self.append_structures(self._structures[index].items())
 
 
 if __name__ == '__main__':
-    depos = [DepoStructure('/home/ydanilovsky@nolabel.local/PycharmProjects/Depo/tests/depo_project_test'),
-             DepoStructure('/home/ydanilovsky@nolabel.local/PycharmProjects/Depo/tests/depo_project_test_second'),
-             DepoStructure('/home/ydanilovsky@nolabel.local/PycharmProjects/Depo/tests/depo_project_test_third')]
-    project = DepoCombain(depos)
-    items = project.items
-    print(items)
-    # print(items.name)
+    depos = [DepoStructure('/home/ydanilovsky@nolabel.local/PycharmProjects/Depo/tests/depo_project_1'),
+             DepoStructure('/home/ydanilovsky@nolabel.local/PycharmProjects/Depo/tests/depo_project_2'),
+             DepoStructure('/home/ydanilovsky@nolabel.local/PycharmProjects/Depo/tests/depo_project_3')]
+    project = Depo(depos)
+    itm = project.items()
+    for i in itm:
+        print(i.parent)
