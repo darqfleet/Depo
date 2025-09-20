1a83cbfa86685d77d667c3839c8b9db97e4c087c
darqfleet <darqfleet@gmail.com>
2025-09-20 14:15:23 +0200

    refactoring to pathlib

---
+++ shell.nix
@@ -0,0 +1,10 @@
+let 
+  pkgs = import <nixpkgs> { };
+in
+  pkgs.mkShell {
+    packages = with pkgs;  [
+      inetutils
+      jetbrains.pycharm-professional
+      poetry
+    ];
+}
--- src/depo/utils.py
+++ src/depo/utils.py
@@ -1,8 +1,9 @@
 import pathlib
 from exceptions import *
+from pathlib import Path
 
-def exist_path(path: str):
+def exist_path(path: Path):
     message = f'{path}, not exist'
-    if not pathlib.Path(path).exists():
+    if not path.exists():
         raise Missing(message)
     return True
