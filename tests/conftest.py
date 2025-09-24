33af8e37c6f295f8f4d8b219e7c7f10114b14bd9
darqfleet <yaroslav.danilovsky@gmail.com>
2025-09-24 15:34:08 +0200

    implement new classes, Step, AbstractDepoUnit

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,11 +6,11 @@ import os
 @pytest.fixture
 def depo_structure():
 
-    depos = [DepoStructure(f'depo_project_1'),
-             DepoStructure(f'depo_project_2'),
-             DepoStructure(f'depo_project_3')]
+    depos = [Depo(f'depo_project_1'),
+             Depo(f'depo_project_2'),
+             Depo(f'depo_project_3')]
     return depos
 
 @pytest.fixture
 def depo(depo_structure):
-    return Depo(depo_structure)
+    return DepoCombaine(depo_structure)
--- tests/test_depo_core.py
+++ tests/test_depo_core.py
@@ -2,14 +2,14 @@ from depo.core import DepoItem
 
 class TestDepo:
     def test_current_default(self, depo, depo_structure):
-        assert depo.current_structure == depo_structure[0]
+        assert depo.current_depo == depo_structure[0]
 
     def test_length(self, depo):
-        assert len(depo.structures()) == 3
+        assert len(depo.all_depos()) == 3
 
     def test_length_items(self, depo):
         assert len(depo.items()) == 9
 
     def test_items(self, depo):
-        item = depo.item('0030')
+        item = depo.find('0030')
         assert isinstance(item, DepoItem)
