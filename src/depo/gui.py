0e159d0953bfec5418813caa39ac936d1ba5826c
darqfleet <yaroslav.danilovsky@gmail.com>
2025-10-02 17:31:08 +0200

    experimenting with gui

--- src/depo/gui.py
+++ src/depo/gui.py
@@ -1,50 +1,72 @@
-import pathlib
 import sys
-
-from PySide6.QtWidgets import QWidget, QApplication, QVBoxLayout, QHBoxLayout, QLabel, QMainWindow, QToolBar
+import pathlib
+from PySide6.QtWidgets import QWidget, QApplication, QVBoxLayout, QHBoxLayout, QLabel, QMainWindow, QToolBar, QSplitter, QPushButton, QComboBox, QStackedWidget
 from PySide6.QtCore import Qt
 from PySide6.QtGui import QPixmap, QAction, QIcon
 
-icon_path = pathlib.Path(__file__).parent.parent.parent / 'icons'
 
 class Filler(QWidget):
-    def __init__(self, icon='nuke.svg', *args, **kwargs):
+    def __init__(self, text, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.icon = icon
-        self.init_ui()
-    def init_ui(self):
-        self.set_window_widgets()
-    def set_window_widgets(self):
-        label = QLabel()
-        logo_pix = QPixmap(f'{icon_path}/{self.icon}')
-        label.setPixmap(logo_pix)
+        label = QLabel(text)
         label.setAlignment(Qt.AlignmentFlag.AlignCenter)
         layout_main = QVBoxLayout()
         layout_main.addWidget(label)
-        # self.setLayout(layout_main)
+        self.setLayout(layout_main)
 
 
 class Window(QMainWindow):
     def __init__(self, parent=None, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.parent = parent
-        self.init_ui()
-
-    def init_ui(self):
-        self.setWindowTitle('Test Slice')
         self.setup_window()
-        self.setCentralWidget(Filler())
 
 
     def setup_window(self):
-        self.act_split = QAction(icon=QIcon(f'{icon_path}/nuke.svg'),iconText='Split')
-        self.act_split.triggered.connect(self.split_window)
-        self.toolbar = QToolBar()
-        self.toolbar.addAction(self.act_split)
-        self.addToolBar(self.toolbar)
-    def split_window(self):
-        self.parent.main_layout.addWidget(Window(self.parent))
-        print('split')
+        self.bt_split_hor = QPushButton('SplitH')
+        self.bt_split_hor.clicked.connect(self.split_horizontal)
+        self.bt_split_ver = QPushButton('SplitV')
+        self.bt_split_ver.clicked.connect(self.split_vertical)
+        self.cmb_select = QComboBox()
+
+
+        items = [f'Some widget {x}' for x in range(5)]
+        self.wgts = [Filler(x) for x in items]
+        self.cmb_select.addItems(items)
+        self.stacked = QStackedWidget()
+        [self.stacked.addWidget(x) for x in self.wgts]
+
+
+        layout_top = QHBoxLayout()
+        layout_top.addWidget(self.bt_split_hor)
+        layout_top.addWidget(self.bt_split_ver)
+        layout_top.addWidget(self.cmb_select)
+
+        self.layout_split = QVBoxLayout()
+        self.layout_split.addWidget(self.stacked)
+        layout_main = QVBoxLayout()
+        layout_main.addLayout(layout_top)
+        layout_main.addLayout(self.layout_split)
+        self.cmb_select.currentIndexChanged.connect(self.set_widget)
+
+        container = QWidget()
+        container.setLayout(layout_main)
+
+        self.splitter = QSplitter()
+        self.splitter.addWidget(container)
+        self.setCentralWidget(self.splitter)
+
+    def split_horizontal(self):
+        self.splitter.setOrientation(Qt.Orientation.Horizontal)
+        self.splitter.addWidget(Window())
+
+    def split_vertical(self):
+        self.splitter.setOrientation(Qt.Orientation.Vertical)
+        self.splitter.addWidget(Window())
+
+    def set_widget(self):
+        index = self.cmb_select.currentIndex()
+        self.stacked.setCurrentIndex(index)
 
 class MainAppWindow(QMainWindow):
     def __init__(self):
